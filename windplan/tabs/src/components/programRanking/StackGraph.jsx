ec13e9a8536037adf3ad7612a4f5480843dca14d
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2022-05-24 18:42:41 +0100

    Created buttons file

--- windplan/tabs/src/components/programRanking/StackGraph.jsx
+++ windplan/tabs/src/components/programRanking/StackGraph.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Plot from 'react-plotly.js';
-import Plotly from 'plotly.js';
-import { jsPDF } from "jspdf";
 import { useFind } from 'react-pouchdb';
+import { savePDFbutton } from '../../utils/Buttons';
 
 export function StackGraph() {
 
@@ -16,7 +15,6 @@ export function StackGraph() {
 
   // Costs
   programs.forEach((program, i) => {
-
     let costs = [];
     let k = 0;
 
@@ -42,43 +40,17 @@ export function StackGraph() {
     k++;
   }
 
-  data[programs.length] =
-  {
+  data[programs.length] = {
     x: months,
     y: hours,
     name: "hours",
     type: "scatter"
   }
 
-  let savePDFicon = {
-    'width': 1000,
-    'height': 1000,
-    'path': 'm518 386q0 8-5 13t-13 5q-37 0-63-27t-26-63q0-8 5-13t13-5 12 5 5 13q0 23 16 38t38 16q8 0 13 5t5 13z m125-73q0-59-42-101t-101-42-101 42-42 101 42 101 101 42 101-42 42-101z m-572-320h858v71h-858v-71z m643 320q0 89-62 152t-152 62-151-62-63-152 63-151 151-63 152 63 62 151z m-571 358h214v72h-214v-72z m-72-107h858v143h-462l-36-71h-360v-72z m929 143v-714q0-30-21-51t-50-21h-858q-29 0-50 21t-21 51v714q0 30 21 51t50 21h858q29 0 50-21t21-51z',
-    'transform': 'matrix(1 0 0 -1 0 850)'
-  }
-
-  let savePDFbutton = {
-    name: 'Save plot as PDF',
-    icon: savePDFicon,
-    direction: 'up',
-    click: function (gd) {
-      Plotly.toImage(gd, { format: 'png', height: gd._fullLayout.height, width: gd._fullLayout.width }).then(function (img) {
-        const pdf = new jsPDF({
-          orientation: 'landscape',
-        });
-        const imgProps = pdf.getImageProperties(img);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        pdf.addImage(img, 'PNG', 0, 0, pdfWidth, pdfHeight);
-        pdf.save("program_costs.pdf");
-      })
-    }
-  }
-
   let config = {
     displaylogo: false,
     modeBarButtons: [
-      ['toImage', savePDFbutton],
+      ['toImage', savePDFbutton('Save plot as PDF', 'program_costs')],
       ['zoom2d', 'pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d']
     ]
   }
