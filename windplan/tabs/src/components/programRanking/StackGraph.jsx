6446bcfe124c0c951b6c2f264a30dc1b59aa668d
fuscati <andre@andre-laptop.localdomain>
2022-05-29 22:34:51 +0100

    divide per years

--- windplan/tabs/src/components/programRanking/StackGraph.jsx
+++ windplan/tabs/src/components/programRanking/StackGraph.jsx
@@ -5,6 +5,7 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 export function StackGraph() {
+  var data = []
   const db = useContext(DbContext)
   const [state, setState] = useState({
     programs: [],
@@ -17,11 +18,12 @@ export function StackGraph() {
       const programs = (await db.rel.find('program')).programs
       let months_costs = []
       let program_years = []
-
+      let months_years = []
       for (let program of programs) {
         const items = (await db.rel.find('item', program.items)).items
 
-        let program_months = new Map()
+        let program_months = []
+
         for (let item of items) {
           const projects = (await db.rel.find('project', item.projects)).projects
 
@@ -29,11 +31,15 @@ export function StackGraph() {
             const months = (await db.rel.find('month', project.months)).months
 
             months.forEach((month) => {
-              program_years.push(month["month"].split("-")[0])
-              if (!program_months.has(month["month"]))
-                program_months.set(month["month"], month["cost"])
-              else
-                program_months.set(month["month"], program_months.get(month["month"]) + month["cost"])
+              let year_parsed = parseInt(month["month"].split("-")[0])
+              let month_parsed = parseInt(month["month"].split("-")[1])
+              program_years.push(year_parsed)
+
+              if(program_months[year_parsed]===undefined)
+                program_months[year_parsed]=[]
+              if(program_months[year_parsed][month_parsed]===undefined)
+                program_months[year_parsed][month_parsed]=0
+              program_months[year_parsed][month_parsed]=program_months[year_parsed][month_parsed]+month["cost"]
             })
           }
         }
@@ -51,7 +57,7 @@ export function StackGraph() {
     loadPrograms();
   }, [db])
 
-  console.log(state.years)
+  
   let months_name = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
   var costs_name = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -61,33 +67,37 @@ export function StackGraph() {
     )
   }
 
-  let data = []
-     data.push({
-      x: months_name,
-      y: costs_name,
-      name: "",
-      type: "bar" 
-    }) 
+  
+  data.push({
+    x: months_name,
+    y: costs_name,
+    name: "",
+    type: "bar"
+  })
+
   state.programs.forEach((program, i) => {
     let months = [];
     let costs = [];
 
     if (state.months_costs[i] !== undefined) {
+     
+      state.months_costs[i].forEach((month, year) => {
+      
+        month.forEach((cost, index) => {
+          if(months[year]===undefined)
+            months[year]=[]
 
-      state.months_costs[i].forEach((cost, month) => {
-        if (month.toString().split("-")[0] === state.years[0]) {
+          months[year].push(months_name[index])
+          
+          if(costs[year]===undefined)
+            costs[year]=[]
 
-          // console.log(months_name[parseInt(month.toString().split("-")[1])-1])
-          months.push(months_name[parseInt(month.toString().split("-")[1])-1])
-          costs.push(cost)
-        }
-        
-
+          costs[year].push(cost)
+        })
       })
-
       data.push({
-        x: months,
-        y: costs,
+        x: months[state.years[0]],
+        y: costs[state.years[0]],
         name: program["name"],
         type: "bar"
       })
@@ -111,10 +121,55 @@ export function StackGraph() {
       type: "scatter"
     } */
 
+/* function changeYear(a)
+{
+  data = []
+  data.push({
+    x: months_name,
+    y: costs_name,
+    name: "",
+    type: "bar"
+  })
+
+  console.log("loading")
+  state.programs.forEach((program, i) => {
+    let months = [];
+    let costs = [];
+
+    if (state.months_costs[i] !== undefined) {
+     
+      state.months_costs[i].forEach((month, year) => {
+      
+        month.forEach((cost, index) => {
+          if(months[year]===undefined)
+            months[year]=[]
+
+          months[year].push(months_name[index])
+          
+          if(costs[year]===undefined)
+            costs[year]=[]
+
+          costs[year].push(cost)
+        })
+      })
+      data.push({
+        x: months[state.years[a.value]],
+        y: costs[state.years[a.value]],
+        name: program["name"],
+        type: "bar"
+      })
+
+    }
+  });
+
+  console.log("done")
+
+} */
+
   return (
     <div>
 
-      <Dropdown options={state.years} /* onChange={this._onSelect} */ value={state.years[0]} placeholder="Select an option" />
+      <Dropdown options={state.years}  /* onChange={changeYear} */  value={state.years[0].toString()} placeholder="Select an option" />
       <Plot
         data={data}
         layout={{ height: 400, barmode: 'stack', title: 'Program Costs' }}
